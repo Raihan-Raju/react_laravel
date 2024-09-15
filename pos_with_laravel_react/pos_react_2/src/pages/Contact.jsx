fb511c2f377e6d4cc6b925fddea26b226376f569
Raihan-Raju <idbraihan58@gmail.com>
2024-09-15 15:26:18 +0600

    vue

---
+++ pos_with_laravel_react/pos_react_2/src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Navebar from '../component/Navebar';
+import Footer from '../component/Footer';
+
+const Contact = () => {
+    return (
+        <>
+        <Navebar/>
+             <h1>im contach</h1>
+         <Footer/>
+            
+        </>
+    );
+};
+
+export default Contact;
